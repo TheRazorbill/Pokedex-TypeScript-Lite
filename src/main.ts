ddc1014abe3b59b6672f3c5913d997213f2ac960
Rahian S.S. <ter26621@gmail.com>
2026-06-01 17:36:32 -0300

    feat: adicionar lógica de simulação da Pokédex no arquivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,37 @@
 // criar as interfaces
 // organizar a arquitetura do arquivo
+
+import { buscarPokemon } from "./services/pokeApiService.js";
+import { CatalogoPokemon } from "./services/boxService.js";
+import { TerminalController } from "./controllers/terminalController.js";
+
+async function main() {
+  const catalogo = new CatalogoPokemon();
+
+  console.log("--- Iniciando Simulação da Pokédex TypeScript Lite --- \n");
+
+  const pikachu = await buscarPokemon("pikachu");
+  if (pikachu !== null) {
+    TerminalController.exibirPokemonEncontrado(pikachu);
+    catalogo.adicionar(pikachu);
+  }
+
+  const charmander = await buscarPokemon("charmander");
+  if (charmander !== null) {
+    TerminalController.exibirPokemonEncontrado(charmander);
+    catalogo.adicionar(charmander);
+  }
+
+  const pikachuDuplicado = await buscarPokemon("pikachu");
+  if (pikachuDuplicado !== null) {
+    catalogo.adicionar(pikachuDuplicado);
+  }
+
+  await buscarPokemon("pokemon-inexistente");
+
+  catalogo.listar();
+  catalogo.remover(25);
+  catalogo.listar();
+}
+
+main();
